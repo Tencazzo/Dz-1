afedf8d4f8d83998a1cfbb4a64f6467e60e62174
Admin <Admin@LAPTOP-4U22PCLQ>
2025-03-20 15:01:49 +0300

    Добавьте файлы проекта.

---
+++ Gallery.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Dz_1
+{
+    partial class Gallery
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.ImageListBox = new System.Windows.Forms.ListBox();
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SaveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(12, 13);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(297, 95);
+            this.SaveButton.TabIndex = 0;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(327, 13);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(297, 95);
+            this.LoadButton.TabIndex = 1;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // ImageListBox
+            // 
+            this.ImageListBox.FormattingEnabled = true;
+            this.ImageListBox.ItemHeight = 20;
+            this.ImageListBox.Location = new System.Drawing.Point(12, 134);
+            this.ImageListBox.Name = "ImageListBox";
+            this.ImageListBox.Size = new System.Drawing.Size(297, 304);
+            this.ImageListBox.TabIndex = 2;
+            this.ImageListBox.SelectedIndexChanged += new System.EventHandler(this.ImageListBox_SelectedIndexChanged);
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.Location = new System.Drawing.Point(327, 138);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(442, 268);
+            this.PictureBox.TabIndex = 3;
+            this.PictureBox.TabStop = false;
+            this.PictureBox.Click += new System.EventHandler(this.PictureBox_Click);
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(327, 412);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(442, 26);
+            this.TextBox.TabIndex = 4;
+            this.TextBox.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.PictureBox);
+            this.Controls.Add(this.ImageListBox);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.SaveButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.ListBox ImageListBox;
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.SaveFileDialog SaveFileDialog;
+    }
+}
+
---
+++ Gallery.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dz_1
+{
+    public partial class Gallery : Form
+    {
+        private Dictionary<string, string> imageTitles = new Dictionary<string, string>();
+        private List<string> imagePaths = new List<string>();
+        public Gallery()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    var lines = imagePaths.Select(path => $"{path},{imageTitles[path]}").ToArray();
+                    File.WriteAllLines(saveFileDialog.FileName, lines);
+                }
+            }
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Text Files (*.txt)|*.txt";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    imagePaths.Clear();
+                    imageTitles.Clear();
+                    ImageListBox.Items.Clear();
+
+                    foreach (var line in File.ReadAllLines(openFileDialog.FileName))
+                    {
+                        var parts = line.Split(',');
+                        var path = parts[0];
+                        var title = parts.Length > 1 ? parts[1] : "";
+
+                        imagePaths.Add(path);
+                        imageTitles[path] = title;
+                        ImageListBox.Items.Add(Path.GetFileName(path));
+                    }
+
+                    if (ImageListBox.Items.Count > 0)
+                    {
+                        ImageListBox.SelectedIndex = 0;
+                    }
+                }
+            }
+        }
+
+        private void ImageListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ImageListBox.SelectedIndex >= 0)
+            {
+                var selectedPath = imagePaths[ImageListBox.SelectedIndex];
+                PictureBox.Image = Image.FromFile(selectedPath);
+                TextBox.Text = imageTitles[selectedPath];
+            }
+        }
+
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (ImageListBox.SelectedIndex >= 0)
+            {
+                var selectedPath = imagePaths[ImageListBox.SelectedIndex];
+                imageTitles[selectedPath] = TextBox.Text;
+            }
+        }
+    }
+}
